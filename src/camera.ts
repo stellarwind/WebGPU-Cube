a2b296cbc696a64bca035fb60c8fe00da294424a
stellarwind <folkstorn@gmail.com>
2025-03-21 00:28:37 +0200

    Camera class a bit more flexible

--- src/camera.ts
+++ src/camera.ts
@@ -1,18 +1,36 @@
-import { Mat4, mat4 } from "wgpu-matrix";
+import { Mat4, mat4, vec3, Vec3 } from "wgpu-matrix";
 import { defaultSettings } from "./settings";
 
 export class Camera {
-    fov = (60 * Math.PI) / 180;
-    aspect =
+    private fov: number = (60 * Math.PI) / 180;
+
+    public set setFOV(value: number) {
+        this.fov = (value * Math.PI) / 180;
+    } 
+
+    private aspect =
         defaultSettings.resolution.width / defaultSettings.resolution.height;
-    near = 0.1;
-    far = 1000.0;
+
+    private near = 0.1;
+
+    public set setNear(value: number) {
+        this.near = Math.max(0.001, value);
+    }
+
+    private far = 1000.0;
+
+    public set setFar (value: number) {
+        this.far = Math.max(this.near + 0.001, value);
+    }
     
-    projectionMatrix = mat4.perspective(this.fov, this.aspect, this.near, this.far);
+    private projectionMatrix = mat4.perspective(this.fov, this.aspect, this.near, this.far);
+
+    private cameraPos: Vec3 = vec3.fromValues(0, 0 , 0);
+    private targetPos: Vec3 = vec3.fromValues(0, 0, -1);
     
-    viewMatrix = mat4.lookAt(
-        [0, 0, 0], //eye
-        [0, 0, -1], //target
+    private viewMatrix = mat4.lookAt(
+        this.cameraPos,
+        this.targetPos,
         [0, 1, 0] //up
     );
 
